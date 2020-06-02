b4c76d8c36d65e1d8b7b3296309c1b3112ead6dc
Mira polos <66188045+mira-polos@users.noreply.github.com>
2020-06-02 20:11:28 +0700

    Halo gais halo guys halo gas

---
+++ tambah.sh
@@ -0,0 +1,14 @@
+clear
+sleep 2
+echo "" "" "" ">>SELAMAT DATANG DI TOOLS MIRA TERSAKITI OM DAN TANTE<< "
+echo "" "" "" "gunakan perintah chmod +x tambah untuk mengijinkan"
+echo "" "" "" "" "" "" "" "" "" "" "" "" "gunakan perintah ./tambah.sh"
+echo "------------------------------------------------------------"
+echo
+echo -n "tekan enter jika udah paham boss....."; read tekan enter
+clear
+echo -n "angka1 : "; read a
+
+echo -n "angka2 : "; read b
+n = $[a+b]
+echo -n "jumlah dari $a + $b = "; echo $n
